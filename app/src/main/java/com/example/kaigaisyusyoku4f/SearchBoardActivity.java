232f9befc864cf1a864eb95fe50703d88ac30636
SeongU <inlove369@gmail.com>
2019-06-15 20:02:06 +0900

    글쓰기 페이지, 검색 페이지 추가

--- app/src/main/java/com/example/kaigaisyusyoku4f/SearchBoardActivity.java
+++ app/src/main/java/com/example/kaigaisyusyoku4f/SearchBoardActivity.java
@@ -3,15 +3,22 @@ package com.example.kaigaisyusyoku4f;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 
 import com.example.kaigaisyusyoku4f.R;
 
 public class SearchBoardActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
     public SearchBoardActivity(){
 
     }
@@ -20,5 +27,28 @@ public class SearchBoardActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_board);
+
+        toolbar =(Toolbar) findViewById(R.id.searchToolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setTitle("해외취업 아카데미");
+        toolbar.setSubtitle("검색");
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/kaigaisyusyoku4f/WriteBoardActivity.java
+++ app/src/main/java/com/example/kaigaisyusyoku4f/WriteBoardActivity.java
@@ -2,13 +2,17 @@ package com.example.kaigaisyusyoku4f;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 public class WriteBoardActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +25,9 @@ public class WriteBoardActivity extends AppCompatActivity {
 
         toolbar.setTitle("해외취업 아카데미");
         toolbar.setSubtitle("글작성");
+
+        sendFab();      //글작성 버튼
+
     }
 
     @Override
@@ -31,5 +38,16 @@ public class WriteBoardActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendFab() {
+        fab = (FloatingActionButton) findViewById(R.id.sendFab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
