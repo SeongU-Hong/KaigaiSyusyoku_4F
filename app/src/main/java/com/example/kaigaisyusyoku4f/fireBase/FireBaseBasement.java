3a305950566c70fd64535bdd04d674f1e0f582d7
colucu <colucu@gmail.com>
2019-06-17 10:25:45 +0900

    190617 10:24

--- app/src/main/java/com/example/kaigaisyusyoku4f/fireBase/FireBaseBasement.java
+++ app/src/main/java/com/example/kaigaisyusyoku4f/fireBase/FireBaseBasement.java
@@ -1,32 +1,55 @@
 package com.example.kaigaisyusyoku4f.fireBase;
 
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.example.kaigaisyusyoku4f.models.Board;
+import com.example.kaigaisyusyoku4f.models.Reply;
+import com.google.firebase.database.DatabaseError;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 
 public class FireBaseBasement {
+
     private String id = "";
     private Date today = new Date();
     private SimpleDateFormat date = new SimpleDateFormat("yyyy:MM:dd:hh:mm:ssSS");
     private String title = "";
     private String contents = "";
+
     private String flag = "0";
     private DatabaseReference mDatabase;
 
 
 
-    public void uploadBoard(String id, String title, String contents) {
-        this.id = id;
-        this.title = title;
-        this.contents = contents;
+
+
+    public void uploadBoard(Board board) {
+
         String dateTime = date.format(today);
-        HashMap<String, Object> board = new HashMap<String, Object>();
-        board.put("title", title);
-        board.put("contents", contents);
+        board.setDateTime(dateTime);
+        board.setFlag("0");
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child(id).child(dateTime).setValue(board);
+        mDatabase.child(board.getId()).child(dateTime).setValue(board, new DatabaseReference.CompletionListener() {
+
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                if (databaseError != null) {
+                    System.out.println("Data could not be saved " + databaseError.getMessage());
+                } else {
+                    System.out.println("Data saved successfully.");
+                }
+            }
+        });
+    }
+
+    public void uploadReply(Reply reply) {
+        
+
     }
 }
