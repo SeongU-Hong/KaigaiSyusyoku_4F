d04bd2381259fab662b94d255b35b88752cda5f7
jungjaesung <j8936009@gmail.com>
2019-06-19 16:48:05 +0900

    Merge remote-tracking branch 'origin/U'
    # Conflicts:
    #	app/src/main/java/com/example/kaigaisyusyoku4f/MainActivity.java

--- app/src/main/java/com/example/kaigaisyusyoku4f/fragment/FreeBoard.java
+++ app/src/main/java/com/example/kaigaisyusyoku4f/fragment/FreeBoard.java
@@ -1,6 +1,5 @@
 package com.example.kaigaisyusyoku4f.fragment;
 
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,26 +11,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.kaigaisyusyoku4f.DetailView;
 import com.example.kaigaisyusyoku4f.R;
-import com.example.kaigaisyusyoku4f.VO.FreeboardVO;
 import com.example.kaigaisyusyoku4f.models.Board;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class FreeBoard extends Fragment {
 
@@ -48,7 +41,6 @@ public class FreeBoard extends Fragment {
     }
 
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -62,23 +54,24 @@ public class FreeBoard extends Fragment {
         mAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, List);
         mListView.setAdapter(mAdapter);
 
-        mReference = mDatabase.child ("freeboard");
+
+        mReference = mDatabase.child("freeboard");
         mReference.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                mAdapter.clear();
+//                mAdapter.clear();
                 for (DataSnapshot messageData : dataSnapshot.getChildren()) {
-                    if(messageData.child("freeboard").child("write").exists()){
-                        String msg2 = messageData.child("freeboard").child("write").child("title").getValue().toString();
-                        Log.e(msg2.toString()+"","board :");
-                        List.add(msg2);
-                        mAdapter.add(msg2);
-                    }
+//                    if(messageData.child("freeboard").child("write").exists()){
+                    String msg2 = messageData.child("write").child("title").getValue().toString();
+                    Log.e(msg2 + "", "board :");
+                    List.add(msg2);
+//                    }
                     // child 내에 있는 데이터만큼 반복합니다.
-                    mAdapter.notifyDataSetChanged();
-                    mListView.setSelection(mAdapter.getCount() - 1);
                 }
-
+                mAdapter.add(List);
+                mAdapter.notifyDataSetChanged();
+//                mListView.setSelection(mAdapter.getCount() - 1);
             }
 
 
@@ -93,16 +86,16 @@ public class FreeBoard extends Fragment {
             @Override
             public void onItemClick(AdapterView parent, View view, int i, long l) {
                 Intent intent = new Intent(getContext(), DetailView.class);
-                intent.putExtra("id",String.valueOf(mList.get(i).getId()));
-                intent.putExtra("title",mList.get(i).getTitle());
+                intent.putExtra("id", String.valueOf(mList.get(i).getId()));
+                intent.putExtra("title", mList.get(i).getTitle());
                 //Log.d(mList.get(i).getTitle(),"title");
-                intent.putExtra("contents",mList.get(i).getContents());
+                intent.putExtra("contents", mList.get(i).getContents());
 
-                intent.putExtra("dateTime",String.valueOf(mList.get(i).getDateTime()));
-                mList.get(i).setCount(mList.get(i).getCount()+1);
-                intent.putExtra("count",String.valueOf(mList.get(i).getCount()));
+                intent.putExtra("dateTime", String.valueOf(mList.get(i).getDateTime()));
+                mList.get(i).setCount(mList.get(i).getCount() + 1);
+                intent.putExtra("count", String.valueOf(mList.get(i).getCount()));
                 //Log.d(String.valueOf(mList.get(i).getHitCount()),"hitCount");
-                Toast.makeText(getContext(), "클릭"+mList.get(i).getCount(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "클릭" + mList.get(i).getCount(), Toast.LENGTH_SHORT).show();
                 startActivity(intent);
 
             }
@@ -146,6 +139,7 @@ public class FreeBoard extends Fragment {
         mReference.addChildEventListener(mChild);
 
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
