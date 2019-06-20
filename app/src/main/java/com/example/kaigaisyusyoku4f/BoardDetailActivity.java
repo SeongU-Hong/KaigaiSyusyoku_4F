b901be7e8588852e9679ac58dcde8e3046eb0426
SeongU <inlove369@gmail.com>
2019-06-20 18:06:25 +0900

    성우의 발악3

--- app/src/main/java/com/example/kaigaisyusyoku4f/BoardDetailActivity.java
+++ app/src/main/java/com/example/kaigaisyusyoku4f/BoardDetailActivity.java
@@ -14,6 +14,8 @@ public class BoardDetailActivity extends AppCompatActivity {
 
     }
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
